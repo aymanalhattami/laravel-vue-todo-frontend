35ee1f01e9fe0d124b884300367fd8d5ac75ca65
aymanalhattami <ayman.m.alhattami@gmail.com>
2023-10-17 09:31:11 +0300

    router meta

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
 import routes from "@/router/routes.js";
 
-export default createRouter({
+const router = createRouter({
     routes,
     history: createWebHistory()
-})
+});
+
+router.beforeEach((to, from) => {
+    if(to.meta.auth){
+        return {
+            name: 'login',
+            query: {
+                redirect: to.fullPath
+            }
+        };
+    }
+});
+
+export default router;
