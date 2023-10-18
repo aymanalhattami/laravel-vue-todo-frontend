923d4fa9a8e678dfbb100c58579ac9d6cf5bfdc0
aymanalhattami <ayman.m.alhattami@gmail.com>
2023-10-18 13:30:22 +0300

    Setup client base authentication

--- src/http/api.js
+++ src/http/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// console.log('import.meta.');
-// console.log(import.meta);
+axios.defaults.withCredentials = true;
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL
