93c4f3959f48fd09cc6104b8990f80ea30ce558a
aymanalhattami <ayman.m.alhattami@gmail.com>
2023-10-19 16:57:24 +0300

    fix: register using api not spa

--- src/http/auth-api.js
+++ src/http/auth-api.js
@@ -4,7 +4,7 @@ export const csrfCookie = () => api.get("/sanctum/csrf-cookie");
 
 export const login = (credentials) => api.post("/api/auth/login", credentials);
 
-export const register = (user) => api.post("/auth/register", user);
+export const register = (user) => api.post("/api/auth/register", user);
 
 export const logout = (token) => {
     return api.post("api/auth/logout", {}, {
